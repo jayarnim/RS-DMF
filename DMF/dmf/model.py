a122bdffb423d07fdc35b7eeeb5360a9770da144
jayarnim <jayarnim727@gmail.com>
2025-09-20 18:35:11 +0900

    del device

--- DMF/dmf/model.py
+++ DMF/dmf/model.py
@@ -18,16 +18,15 @@ class Module(nn.Module):
         del self.init_args["self"]
         del self.init_args["__class__"]
 
-        # device setting
-        DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-        self.device = torch.device(DEVICE)
-
         # global attr
         self.n_users = n_users
         self.n_items = n_items
         self.hidden = hidden
         self.dropout = dropout
-        self.interactions = interactions.to(self.device)
+        self.register_buffer(
+            name="interactions", 
+            tensor=interactions,
+        )
 
         # generate layers
         self._init_layers()
@@ -41,10 +40,7 @@ class Module(nn.Module):
         user_idx: (B,)
         item_idx: (B,)
         """
-        user_idx = user_idx.to(self.device)
-        item_idx = item_idx.to(self.device)
-        logit = self.score(user_idx, item_idx)
-        return logit
+        return self.score(user_idx, item_idx)
 
     def predict(
         self, 
@@ -55,13 +51,9 @@ class Module(nn.Module):
         user_idx: (B,)
         item_idx: (B,)
         """
-        user_idx = user_idx.to(self.device)
-        item_idx = item_idx.to(self.device)
-
         with torch.no_grad():
             logit = self.score(user_idx, item_idx)
             pred = torch.sigmoid(logit)
-
         return pred
 
     def score(self, user_idx, item_idx):
